dea107ab2352062937cbc848e8fe56aa97711014
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-02-08 00:34:15 +0530

    learn context api with second project

--- learn11/src/App.jsx
+++ learn11/src/App.jsx
@@ -1,19 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Themeprovider } from "./context/Theme";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/*  */}
-          </div>
+      <Themeprovider value={{ themeMode, lightTheme, darkTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
 
-          <div className="w-full max-w-sm mx-auto">
-            {/*  */}
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
           </div>
         </div>
-      </div>
+      </Themeprovider>
     </>
   );
 }
--- learn11/src/context/Theme.js
+++ learn11/src/context/Theme.js
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 export const ThemeContext = createContext({
   themeMode: "light",
   darkTheme: () => {},
-  LightTheme: () => {},
+  lightTheme: () => {},
 });
 
 export const Themeprovider = ThemeContext.Provider;
